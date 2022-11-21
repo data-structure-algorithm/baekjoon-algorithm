359ed7ecfdc9c2ca7fd900d3d250a5822fa5e0d1
Jongco <narira0922@gmail.com>
2022-11-21 12:57:21 +0900

    feat: array로 기본적인 스택 구현

--- stack/Jonghyun/implement.js
+++ stack/Jonghyun/implement.js
@@ -1,38 +1,57 @@
 //stack.js
 
 class Stack {
+  #array = [];
+
+  constructor() {}
+
   /**
    * 값을 넣는 함수
    */
-  push() {
-    //
+  push(value) {
+    this.#array.push(value);
   }
 
   /**
    * 마지막 요소를 반환하고 제거하는 함수
    */
   pop() {
-    //
+    return this.#array.pop();
   }
 
   /**
    * 마지막 요소를 반환하는 함수
    */
   peek() {
-    //
+    return this.#array[this.#array.length - 1];
   }
 
   /**
    * 현재 사이즈를 반환하는 함수
    */
   size() {
-    //
+    return this.#array.length;
   }
 
   /**
    * Stack이 꽉 차있는지 여부를 반환하는 함수
    */
   isEmpty() {
-    //
+    let result = false;
+    if (this.#array.length === 0) {
+      result = true;
+    }
+    return result;
   }
 }
+
+const stack = new Stack();
+
+stack.push("🍎");
+stack.push("🍗");
+stack.push("🍕");
+stack.push("🍔");
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
