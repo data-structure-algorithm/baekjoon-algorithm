0a67797ce7bced393397a977c044bcf676041143
Jongco <narira0922@gmail.com>
2022-11-27 16:38:26 +0900

    feat: Queue rear, front, peek 구현 및 maxSize 넣어 Queue가 꽉 차면 에러 메세지 반환

--- queue/Jonghyun/implement.js
+++ queue/Jonghyun/implement.js
@@ -3,46 +3,79 @@
  * 1. Enqueue : 맨 뒤에 추가
  * 2. Dequeue : 맨 앞에 삭제 => 데이터 반환
  * 3. Peek : 맨 앞에 (front) 에 위치한 데이터를 읽음
- * 5. rear : 맨 뒤의 위치한 데이터를 가리키는 포인터
+ * 4. front : 맨 앞의 위치(index)
+ * 5. rear : 맨 뒤의 위치(index)
  * 6. isEmpty : 큐가 비었는지 확인하는 함수
+ * 7. tail : 맨 뒤의 위치한 데이터를 가리키는 포인터
  */
 
 class Queue {
-  constructor() {
-    this.node = { value: undefined, next: undefined };
-    this.size = 0;
-    this.head = { value: undefined, next: undefined };
-    this.pointer;
+  #rear;
+  #size;
+  #head;
+  #front;
+
+  constructor(maxSize) {
+    this.maxSize = maxSize;
+    this.#head = { value: undefined, next: undefined };
+    this.#size = 0;
+    this.#front = 0;
+    this.#rear = 0;
+    this.tail;
   }
 
   isEmpty() {
-    return this.size === 0;
+    return this.#size === 0;
+  }
+
+  isFull() {
+    return this.#size === this.maxSize;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  get rear() {
+    if (this.#size === 0) throw new Error("텅 빔");
+    return this.#rear;
+  }
+
+  get front() {
+    if (this.#size === 0) throw new Error("텅 빔");
+    return this.#front;
   }
 
   enqueue(value) {
+    if (this.isFull()) throw new Error("꽉 참");
     const node = { value, next: undefined };
     if (this.isEmpty()) {
-      this.head = node;
-      this.rear = node;
+      this.#head = node;
+      this.tail = node;
     } else {
-      this.rear.next = node;
-      this.rear = this.rear.next;
+      this.tail.next = node;
+      this.tail = this.tail.next;
     }
-    this.size++;
+    this.#rear++;
+    this.#size++;
   }
 
   dequeue() {
     if (this.isEmpty()) throw new Error("텅 빔");
-    const node = this.head;
-    this.head = this.head.next;
-    this.size--;
+    const node = this.#head;
+    this.#head = this.#head.next;
+    this.#front++;
+    this.#size--;
     return node;
   }
 
-  peek() {}
+  peek() {
+    if (this.isEmpty()) throw new Error("텅 빔");
+    return this.#head.value;
+  }
 }
 
-const queue = new Queue();
+const queue = new Queue(5);
 queue.enqueue("apple");
 queue.enqueue("banana");
 queue.enqueue("orange");
@@ -51,4 +84,8 @@ queue.enqueue("chicken");
 console.log(queue.dequeue());
 console.log(queue.dequeue());
 console.log(queue.dequeue());
+console.log(queue.peek());
+console.log(queue.front);
+console.log(queue.rear);
+console.log(queue.peek());
 console.log(queue.dequeue());
