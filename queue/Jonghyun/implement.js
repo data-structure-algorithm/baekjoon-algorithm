c4f1af4fe498e8156ada71b7cf0c86351ed1e948
Jongco <narira0922@gmail.com>
2022-11-27 16:20:25 +0900

    feat: isEmpty 함수 생성 및 리팩터링

--- queue/Jonghyun/implement.js
+++ queue/Jonghyun/implement.js
@@ -3,24 +3,25 @@
  * 1. Enqueue : 맨 뒤에 추가
  * 2. Dequeue : 맨 앞에 삭제 => 데이터 반환
  * 3. Peek : 맨 앞에 (front) 에 위치한 데이터를 읽음
- * 4. front : 맨 앞의 위치 => 인덱스
- * 5. rear : 맨 뒤의 위치 => 인덱스
+ * 5. rear : 맨 뒤의 위치한 데이터를 가리키는 포인터
  * 6. isEmpty : 큐가 비었는지 확인하는 함수
  */
 
 class Queue {
   constructor() {
     this.node = { value: undefined, next: undefined };
-    this.front = 0;
-    this.rear;
     this.size = 0;
     this.head = { value: undefined, next: undefined };
     this.pointer;
   }
 
+  isEmpty() {
+    return this.size === 0;
+  }
+
   enqueue(value) {
     const node = { value, next: undefined };
-    if (this.size == 0) {
+    if (this.isEmpty()) {
       this.head = node;
       this.rear = node;
     } else {
@@ -31,7 +32,7 @@ class Queue {
   }
 
   dequeue() {
-    if (this.size === 0) throw new Error("텅 빔");
+    if (this.isEmpty()) throw new Error("텅 빔");
     const node = this.head;
     this.head = this.head.next;
     this.size--;
@@ -51,6 +52,3 @@ console.log(queue.dequeue());
 console.log(queue.dequeue());
 console.log(queue.dequeue());
 console.log(queue.dequeue());
-console.log(queue.dequeue());
-
-// console.log(queue);
